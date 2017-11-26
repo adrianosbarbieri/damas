ac464ca0509d79bcf9839165aa8d348af90bfad2
Adriano <adriano2147@gmail.com>
2017-11-26 20:03:09 -0200

    LEDS!

--- damas.ino
+++ damas.ino
@@ -1,3 +1,8 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
 void setup();
 void copy_matrix();
 int scan_matrix();
@@ -7,6 +12,7 @@ void threat_square(int x, int y);
 void convert_to_queen();
 void convert_input_matrix();
 void start_game();
+void print_leds();
 bool check_threat_pp(int x, int y);
 bool check_threat_pn(int x, int y);
 bool check_threat_nn(int x, int y);
@@ -27,17 +33,31 @@ enum PECA
 int matrix_old[8][8] = {{A, E, A, E, E, E, B, E}, {E, A, E, E, E, B, E, B}, {A, E, A, E, E, E, B, E},
                         {E, A, E, E, E, B, E, B}, {A, E, A, E, E, E, B, E}, {E, A, E, E, E, B, E, B},
                         {A, E, A, E, E, E, B, E}, {E, A, E, E, E, B, E, B}};
+
 int matrix[8][8] = {{0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
                     {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
                     {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}};
+
 int input_matrix[8][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0},
                           {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+
+int led_matrix[8][8] = {{-1, 2, -1, 4, -1, 6, -1, 8},
+                        {13, -1, 15, -1, 17, -1, 19, -1},
+                        {-1, 23, -1, 25, -1, 27, -1, 29},
+                        {33, -1, 35, -1, 37, -1, 39, -1},
+                        {-1, 43, -1, 45, -1, 47, -1, 49},
+                        {53, -1, 55, -1, 57, -1, 59, -1},
+                        {-1, 63, -1, 65, -1, 67, -1, 69},
+                        {73, -1, 75, -1, 77, -1, 79, -1}};
+
 bool curr_turn; /* true = A, false = B */
 bool last_turn; /* true = A, false = B */
-const int lin[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-const int col[4] = {10, 11, 12, 13}; // 12, 13};
+const int col[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+const int lin[4] = {10, 11, 12, 13}; // 12, 13};
 const int turn_read = 0;
 const int turn_write = 1;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(80, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
@@ -46,22 +66,51 @@ void setup()
     Serial.println();
     for(int i = 0; i < 8; i++)
     {
-        Serial.print("linha: ");
-        Serial.print(lin[i]);
+        Serial.print("coluna: ");
+        Serial.print(col[i]);
         Serial.println();
-        pinMode(lin[i], OUTPUT);
+        pinMode(col[i], OUTPUT);
     }
 
     for(int i = 0; i < 4; i++)
     {
-        Serial.print("coluna: ");
-        Serial.print(col[i]);
+        Serial.print("linha: ");
+        Serial.print(lin[i]);
         Serial.println();
-        pinMode(col[i], INPUT);
+        pinMode(lin[i], INPUT);
     }
     pinMode(turn_write, OUTPUT);
     pinMode(turn_read, INPUT);
     digitalWrite(turn_write, HIGH);
+    #if defined (__AVR_ATtiny85__)
+      if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+    #endif
+    pixels.begin();
+}
+
+void print_leds()
+{
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            if(led_matrix[i][j] > 0)
+            {
+                switch(led_matrix[i][j])
+                {
+                    case A:
+                      pixels.setPixelColor(led_matrix[i][j], pixels.Color(0, 0, 25));
+                      break;
+                    case B:
+                      pixels.setPixelColor(led_matrix[i][j], pixels.Color(25, 0, 0));
+                      break;
+                    case E:
+                      pixels.setPixelColor(led_matrix[i][j], pixels.Color(25, 25, 25));
+                      break;
+                }
+            }
+        }
+    }
 }
 
 void copy_matrix()
@@ -89,15 +138,15 @@ int scan_matrix()
     clear_input_matrix();
     for(int i = 0; i < 8; i++)
     {
-        digitalWrite(lin[i], HIGH);
+        digitalWrite(col[i], HIGH);
         for(int j = 0; j < 4; j++)
         {
-            if(digitalRead(col[j]) == HIGH)
+            if(digitalRead(lin[j]) == HIGH)
                 input_matrix[i][j] = 1;
             else
                 input_matrix[i][j] = 0;
         }
-        digitalWrite(lin[i], LOW);
+        digitalWrite(col[i], LOW);
     }
 
     convert_input_matrix();
@@ -191,6 +240,7 @@ int scan_matrix()
     convert_to_queen();
     if(curr_turn != last_turn)
         copy_matrix();
+    print_leds();
 }
 
 void start_game()
@@ -200,18 +250,18 @@ void start_game()
     Serial.println();
     for(int i = 0; i < 8; i++)
     {
-        Serial.print("linha: ");
-        Serial.print(lin[i]);
+        Serial.print("coluna: ");
+        Serial.print(col[i]);
         Serial.println();
-        pinMode(lin[i], OUTPUT);
+        pinMode(col[i], OUTPUT);
     }
 
     for(int i = 0; i < 4; i++)
     {
-        Serial.print("coluna: ");
-        Serial.print(col[i]);
+        Serial.print("linha: ");
+        Serial.print(lin[i]);
         Serial.println();
-        pinMode(col[i], INPUT);
+        pinMode(lin[i], INPUT);
     }
 }
 
@@ -247,13 +297,13 @@ bool check_threat_pp(int x, int y)
     {
         if(matrix[x + 1][y + 1] == BK && matrix[x + 2][y + 2] == E)
         {
-            return true;
             matrix[x + 1][y + 1] = BKT;
+            return true;
         }
         if(matrix[x + 1][y + 1] == B && matrix[x + 2][y + 2] == E)
         {
-            return true;
             matrix[x + 1][y + 1] = BT;
+            return true;
         }
     }
     return false;
@@ -390,9 +440,9 @@ void print_input_matrix()
 void loop()
 {
     for(int i = 0; i < 8; i++)
-        digitalWrite(lin[i], LOW);
-    for(int i = 0; i < 4; i++)
         digitalWrite(col[i], LOW);
+    for(int i = 0; i < 4; i++)
+        digitalWrite(lin[i], LOW);
     scan_matrix();
     for(int i = 0; i < 8; i++)
         for(int j = 0; j < 8; j++)
